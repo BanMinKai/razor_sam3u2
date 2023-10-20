ae0ba0205496e98b69a7c9c72330c0838243efec
BanMinKai <khaiphan4949@gmail.com>
2023-10-19 20:13:52 -0600

    LED BASIC
    2023-10-19th

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -92,6 +92,20 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
+  // led off all unused LEDs
+  LedOff(CYAN);
+  LedOff(GREEN);
+  LedOff(YELLOW);
+  LedOff(ORANGE);
+  LedOff(WHITE);
+  LedOff(PURPLE);
+  LedOff(BLUE);
+  LedOff(RED);
+  
+  // backlight to white
+  LedOff(LCD_RED);
+  LedOff(LCD_GREEN);
+  LedOff(LCD_BLUE);
   /* If good initialization, set state to Idle */
   if( 1 )
   {
@@ -140,6 +154,116 @@ State Machine Function Definitions
 /* What does this state do? */
 static void UserApp1SM_Idle(void)
 {
+  static u16 u16BlinkCount = 0;
+  static u8 u8Counter = 0;
+  static u8 u8ColorIndex;
+  
+  u16BlinkCount++;
+  if (u16BlinkCount == 250)
+  {
+      u16BlinkCount = 0;
+      
+      // add a u8 counter
+      u8Counter++;
+      if (u8Counter == 16)
+      {
+          u8Counter = 0;
+      }
+      
+      // masking red
+      if (u8Counter & 0x01)
+      {
+          LedOn(RED);
+      }
+      else 
+      {
+          LedOff(RED);
+      }
+      
+      // masking orange
+      if (u8Counter & 0x02) // similar to check if the bit 1 is HIGH (1)
+      {
+          LedOn(ORANGE);
+      }
+      else 
+      {
+          LedOff(ORANGE);
+      }
+      
+      // masking yellow
+      if (u8Counter & 0x04) // similar to check if bit 3 is HIGH (1)
+      {
+          LedOn(YELLOW);
+      }
+      else 
+      {
+          LedOff(YELLOW);
+      }
+      
+      // masking green
+      if (u8Counter & 0x08)
+      {
+          LedOn(GREEN);
+      }
+      else 
+      {
+          LedOff(GREEN);
+      }
+      
+      // u8ColorIndex stuff
+      u8ColorIndex++;
+      if (u8ColorIndex == 7)
+      {
+          u8ColorIndex = 0;
+      }
+      
+      // set backlight colors using color mixing
+      switch (u8ColorIndex)
+      {
+      case 0: // white
+        LedOn(LCD_RED);
+        LedOn(LCD_GREEN);
+        LedOn(LCD_BLUE);
+        break;
+      
+      case 1: // purple
+        LedOn(LCD_RED);
+        LedOff(LCD_GREEN);
+        break;
+        
+      case 2: // blue
+        LedOn(LCD_BLUE);
+        LedOff(LCD_RED);
+        LedOff(LCD_GREEN);
+        break;
+        
+      case 3: // cyan
+          LedOn(LCD_GREEN);
+          LedOn(LCD_BLUE);
+          LedOff(LCD_RED);
+          break;
+          
+      case 4: // green
+          LedOn(LCD_GREEN);
+          LedOff(LCD_RED);
+          LedOff(LCD_BLUE);
+          break;
+          
+      case 5: // yellow
+          LedOn(LCD_GREEN);
+          LedOn(LCD_RED);
+          LedOff(LCD_BLUE);
+          break;
+          
+      case 6: // red
+        LedOn(LCD_RED);
+        LedOff(LCD_BLUE);
+        LedOff(LCD_GREEN);
+        break;
+      }
+      
+  }
+  
     
 } /* end UserApp1SM_Idle() */
      
